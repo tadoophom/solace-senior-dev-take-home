5475230b33a7746199b4ecbac20f8db537be2af0
Aktan Azat <aazat@ucdavis.edu>
2025-07-15 00:23:53 -0700

    feat(task-B): add browser VAD helper and env-aware VAD facade

--- task-B/src/index.js
+++ task-B/src/index.js
@@ -2,7 +2,7 @@
  * @module @solace/client-sdk
  */
 const { encryptBlob, decryptBlob } = require('./crypto');
-const { recordAndDetectVoice } = require('./vad');
+const { recordAndDetectVoice } = require('./vad/index');
 const { uploadBlob, downloadAndDecrypt } = require('./http');
 
 module.exports = {
